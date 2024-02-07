c1bec56c51497201083bfeef44724a7f9bdd05c3
Darlene-M <moundoungadarlene@gmail.com>
2024-02-07 02:01:29 +0100

    Connecting MongoDB to the API

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,17 @@
+require('dotenv').config()
 const express = require("express")
+const mongoose = require("mongoose")
+
+
+//Connexion à la bd
+const url = `mongodb+srv://darlene:${process.env.DATABASE_PASSWORD}@cluster.rnslr8s.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log("Connexion à la base de donnée réussie"))
+.catch(()=> console.log("Connexion à la base de donnée échouée"))
 const app = express()
 
 app.use((req,res)=>{
