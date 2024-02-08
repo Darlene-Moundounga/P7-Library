9928af60935543b5cca5003c96c1acfe31522043
Darlene-M <moundoungadarlene@gmail.com>
2024-02-08 16:40:37 +0100

    Add user connexion

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const User = require("./models/User")
 const bcrypt= require("bcrypt")
-
+const jwt = require("jsonwebtoken")
+const secretKey = require("./key")
 
 //Connexion à la bd
 const url = `mongodb+srv://darlene:${process.env.DATABASE_PASSWORD}@cluster.rnslr8s.mongodb.net/?retryWrites=true&w=majority`
@@ -29,6 +30,18 @@ async function hashPassword(password) {
     return await bcrypt.hash(password, saltRounds)
 }
 
+async function verifyPassword(password,hashedPassword){
+    return await bcrypt.compare(password,hashedPassword)
+}
+
+function generateToken(id,secret){
+    return jwt.sign(
+        {userId:id},
+        secret,
+        {expiresIn:"1h"}
+        
+    )
+}
 // middleware user inscription 
 app.post("/api/auth/signup", async (req, res) => { //fonction async, car hasPassword est asynchrone
     const user = new User({ //création d'un nouvel user avec ses propriétés
@@ -41,6 +54,31 @@ app.post("/api/auth/signup", async (req, res) => { //fonction async, car hasPass
   });
 
 
+// middleware user login
+app.post("/api/auth/login", (req,res)=>{
+    //récupérer l'email envoyé et de vérifier si un utilisateur existe avec cet email
+    User.findOne({email: req.body.email})
+    .then(
+        async (user) => { 
+            if(user){ // dans ce cas on vérifie le mot de passe
+                const matchPassword = await verifyPassword(req.body.password,user.password)
+                if(matchPassword){ //les password correspondent donc générer le token 
+                    const token = generateToken(user._id,secretKey)
+                    res.json({userId: user._id, token})
+                }else{ //si le mdp est incorrect
+                    res.status(401).json({message: "Identifiants incorrects"}) //unauthorized
+                }
+            }else{
+                res.status(404).json({message: "Utilisateur introuvable"}) //not found
+            }
+        }
+    )
+    .catch(error => res.status(404).json({error}))
+    //si l'email existe
+    //vérifie les password, sont ils les mêmes?
+    //si password ok => générer un token (avec userId)
+    //connecte
+})
 
 
 module.exports  = app
