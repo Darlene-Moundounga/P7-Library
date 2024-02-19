3348c7d06d2f6e2f33f40b78ebeee2497b4398dc
Darlene-M <moundoungadarlene@gmail.com>
2024-02-19 15:11:42 +0100

    Add DELETE and PUT

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -29,10 +29,44 @@ exports.addBook = (req, res) => {
     const book = new Book({
         ...bookData,
         imageUrl: imgURL,
-        userId: req.auth.userId
+        userId: req.auth.userId,
     })
     book.save()
     .then( () => res.status(201).json({message : "Livre enregistré !"}))
     .catch( error => res.status(400).json({ error }))
+    
+}
+
+exports.deleteBook = (req,res) =>{
+    Book.deleteOne({_id:req.params.id})
+    .then( () => res.json({message : "Livre supprimé !"}))
+    .catch( error => res.status(400).json({ error }))
+}
+
+exports.updateBook =  (req, res) => {
+    updateBookById(req, res)
+}
+
+const updateBookById =  (req, res) => {
+    if(!req.file){
+        req.auth.userId === req.body.userId ? updateRequest(req, res, {...req.body}) : res.status(403).json({message: "Unauthorized request"})
+    } else {
+        const bookData = JSON.parse(req.body.book)
+        const imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+        req.auth.userId === bookData.userId ? updateRequest(req, res, {...bookData, imageUrl}) : res.status(403).json({message: "Unauthorized request"})
+    }
+}
+
+const updateRequest = (req, res, params) => {
+    Book.updateOne({_id: req.params.id}, params)
+    .then( () => res.json({message: "Livre modifié !"}))
+    .catch( error => res.status(400).json({ error }))
+}
+
+
+exports.getBestRatingBooks = (req, res) => {
+    Book.find().sort({averageRating: -1}).limit(3)
+    .then( (books) => {res.send(books)})
+    .catch(error => res.status(400).json( { error }))
     
 }
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -5,8 +5,17 @@ const multer = require('../middleware/multer-config')
 const auth = require('../middleware/auth')
 
 router.get("/", bookCtrl.getAllBooks)
+
+router.get("/bestrating", bookCtrl.getBestRatingBooks)
+
 router.get("/:id", bookCtrl.getOneBook)
 
 router.post("/", auth, multer, bookCtrl.addBook)
+
+router.delete("/:id", auth, bookCtrl.deleteBook)
+
+router.put("/:id", auth, multer, bookCtrl.updateBook)
+
+
 
 module.exports = router
